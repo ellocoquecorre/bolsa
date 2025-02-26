d182640aa0901414db2c9902e3ba9dcc0cb1a864
Pablo <paimarino@gmail.com>
2025-02-26 10:19:17 -0300

    Subiendo

--- public_html/backend/cliente.php
+++ public_html/backend/cliente.php
@@ -436,6 +436,7 @@ $cliente_id = isset($_GET['cliente_id']) ? $_GET['cliente_id'] : 1;
     <script src="../js/eliminar_acciones.js"></script>
     <script src="../js/ingresar_efectivo.js"></script>
     <script src="../js/formato_miles_balance.js"></script>
+    <script src="../js/retirar_efectivo.js"></script>
     <!-- FIN JS -->
 </body>
 

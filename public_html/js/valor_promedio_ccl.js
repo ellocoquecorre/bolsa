7cafabef887fb09bbd60666290a8978fea52ea15
Pablo <paimarino@gmail.com>
2025-03-07 15:56:58 -0300

    Subiendo

--- public_html/js/valor_promedio_ccl.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('#btnAccionesDolares').on('click', function() {
-        $('#valorPromedioCCL').removeClass('d-none');
-    });
-    $('#btnAccionesPesos').on('click', function() {
-        $('#valorPromedioCCL').addClass('d-none');
-    });
-});

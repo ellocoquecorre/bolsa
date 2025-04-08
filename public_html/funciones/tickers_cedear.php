58fe6ef7ef347d3e18040694e53a2ca0c878f9bc
Pablo <paimarino@gmail.com>
2025-04-08 18:42:17 -0300

    Subiendo

--- public_html/funciones/tickers_cedear.php
+++ public_html/funciones/tickers_cedear.php
@@ -3,20 +3,13 @@ require_once '../../config/config.php';
 
 if (isset($_GET['term'])) {
     $term = $_GET['term'] . '%';
-    $sql = "SELECT ticker, company_name FROM tickers_cedears WHERE ticker LIKE ? LIMIT 10";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("s", $term);
-    $stmt->execute();
-    $stmt->bind_result($ticker, $company_name);
 
-    $results = [];
-    while ($stmt->fetch()) {
-        $results[] = [
-            'ticker' => $ticker,
-            'company_name' => $company_name
-        ];
-    }
-    $stmt->close();
+    $sql = "SELECT ticker, company_name FROM tickers_cedears WHERE ticker LIKE :term LIMIT 10";
+    $stmt = $conexion->prepare($sql);
+    $stmt->bindParam(':term', $term, PDO::PARAM_STR);
+    $stmt->execute();
+
+    $results = $stmt->fetchAll();
 
     echo json_encode($results);
 }
